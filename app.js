94be3deae318ee2ee5ecd22144b2b185fb8d9169
Yusuf Ganiyu <airscholar@gmail.com>
2020-10-01 22:45:44 +0100

    Initialize Repository. Added express backend, google auth, passport, mongoose, morgan, handlebars and others. Created mongoose model and connected to mongoose db

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const passport = require('passport')
+const session = require('express-session');
+
+
+const connectDB = require('./config/db');
+
+//load config
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//connect db
+connectDB();
+
+//passport config
+require('./config/passport')(passport);
+
+//use morgan
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+//express 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+}))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/index'));
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode and port ${PORT}`));
