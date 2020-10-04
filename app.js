14383539b9546e67374d4df9017afff9795e47d3
Yusuf Ganiyu <airscholar@gmail.com>
2020-10-04 01:02:34 +0100

    Completed edit interface. Used method override to replace POST  with PUT request.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const passport = require('passport')
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const methodOverride = require('method-override');
 // const moment = require('moment');
 const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
 
@@ -21,6 +22,16 @@ const PORT = process.env.PORT || 3000;
 //body parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
 
 //connect db
 connectDB();
--- routes/stories.js
+++ routes/stories.js
@@ -60,7 +60,24 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
 
 });
 
+// @desc Process stories update
+// @route PUT stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    let story = await Story.findById(req.params.id ).lean();
 
+    if (!story) {
+        res.render('errors/404')
+    }
+    // if(story.user !== req.user._id){
+    //     res.render('stories')
+    // } else {
+    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true
+    })
+    res.redirect('/dashboard')
+})
+    // }
 module.exports = router;
 
 
